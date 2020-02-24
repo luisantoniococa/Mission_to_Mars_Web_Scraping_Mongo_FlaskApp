03d1ed7248228b3e6704b355d5d0cb3da997192f
luisantoniococa <luicocurd@gmail.com>
2020-02-23 21:22:44 -0600

    created the mars scrape app.py as an example for another scraper for US states

--- mars_scrape.py
+++ mars_scrape.py
@@ -6,30 +6,35 @@ import time
 
 
 def scrape_info():
-
+    # adding the dictionary from where all the data is going to be shown
     final_dictionary = {}
+    # calling the URL
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-
+    
+    # calling the executable path for Chrome extention
     executable_path = {'executable_path': 'chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=False)
 
     browser.visit(url)
 
     time.sleep(5)
+    
+
+    # getting the html file of the browser 
 
     html = browser.html
     soup = bs(html,'html.parser')
+    # creating a variable to store the article selected
     one_article = soup.find('li', class_='slide')
 
 
-    # for one_article in articles:
-    #     # print(one_article)
-    #     # print('-------------')
 
+    # getting the text and the information required for the data
 
     title_news = one_article.find(class_="content_title").text
     paragraph = one_article.find(class_="article_teaser_body").text
     # print(paragraph)
+    # appending to the final dictionary as with the key latest_news
     final_dictionary['latest_news'] = [title_news,paragraph]
 
 
@@ -105,7 +110,8 @@ def scrape_info():
     browser = Browser('chrome', **executable_path, headless=False)
 
     browser.visit(url)
-
+# -----------------------------------------------------
+# -----------------------------------------------------
 
     hemisphere_image_urls = []
     for x in range(4):
