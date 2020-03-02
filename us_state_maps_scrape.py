f080b11d10b7cd77c329aafc4de99ab3c67b3d75
luisantoniococa <luicocurd@gmail.com>
2020-03-02 11:45:48 -0600

    changed the name of the function for better visibility

--- us_state_maps_scrape.py
+++ us_state_maps_scrape.py
@@ -3,7 +3,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 
-def scrape_info():
+def scrape_():
 
     states_csv = pd.read_csv("../Population_metrics_by_state_D3_visualizations/assets/data/data.csv")
 
